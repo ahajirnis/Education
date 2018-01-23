3a8b3a1e3f8ea1d42437b80f744ce48d29736b36
HAJIRNIS Aniket <L6P@dsone.3ds.com>
2018-01-23 10:55:25 -0500

    Committer: HAJIRNIS Aniket Topic: General re-structuring On branch UdemyCourse Changes to be committed: 	new file:   .gitignore 	new file:   PythonForEverybody/(1)Ni.py 	new file:   PythonForEverybody/(2)rslps.py

---
+++ python/PythonForEverybody/(1)Ni.py
@@ -0,0 +1,7 @@
+# program template for mini-project 0
+
+# Modify the print statement according to 
+# the mini-project instructions
+
+#print "We want... a shrubbery!" # - The Knights who say 'Ni'
+print("We want .... a shrubbery")
