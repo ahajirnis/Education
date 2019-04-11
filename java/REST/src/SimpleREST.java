71661cb31c60f94f7930ea8adac936f3f27d85d6
Aniket Hajirnis <anikethajirnis@gmail.com>
2019-04-11 02:33:27 -0400

    transfer file with gzip encoding

--- java/REST/src/SimpleREST.java
+++ java/REST/src/SimpleREST.java
@@ -4,6 +4,9 @@ import com.sun.net.httpserver.HttpServer;
 
 import java.io.*;
 import java.net.InetSocketAddress;
+import java.util.zip.GZIPOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class SimpleREST
 {
@@ -17,18 +20,40 @@ public class SimpleREST
     static class MyHandler implements HttpHandler
     {
         @Override
-        public void handle(HttpExchange he) throws IOException
+        public void handle(HttpExchange httpExchange) throws IOException
         {
 //            String response = "This is the response";
 //            t.sendResponseHeaders(200, response.length());
             String filePath = "D:\\Projects\\TEST\\RESTTEST\\5secrule.pdf";
-            File canonicalFile = new File(filePath);
-            FileInputStream fis = new FileInputStream(canonicalFile);
-            he.sendResponseHeaders(200, canonicalFile.length());
-            OutputStream os = he.getResponseBody();
-            copyStream(fis, os);
-            os.close();
-            fis.close();
+//            File canonicalFile = new File(filePath);
+//            FileInputStream fis = new FileInputStream(canonicalFile);
+//            he.sendResponseHeaders(200, canonicalFile.length());
+
+//            OutputStream os = he.getResponseBody();
+//            copyStream(fis, os);
+//            os.close();
+//            fis.close();
+
+            InputStream  in = new FileInputStream(filePath);
+            byte[] res = readResource(in,true);
+            httpExchange.getResponseHeaders().set("Content-Encoding", "gzip");
+            httpExchange.sendResponseHeaders(200, res.length);
+            httpExchange.getResponseBody().write(res);
+            httpExchange.getResponseBody().close();
+
+        }
+
+        private static byte[] readResource(InputStream in, boolean gzip) throws IOException {
+            ByteArrayOutputStream bout = new ByteArrayOutputStream();
+            OutputStream gout = gzip ? new GZIPOutputStream(bout) : new DataOutputStream(bout);
+            byte[] tmp = new byte[4096];
+            int r;
+            while ((r=in.read(tmp)) >= 0)
+                gout.write(tmp, 0, r);
+            gout.flush();
+            gout.close();
+            in.close();
+            return bout.toByteArray();
         }
 
         private void copyStream(InputStream is, OutputStream os) throws IOException {
