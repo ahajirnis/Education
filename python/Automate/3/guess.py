9d5a20898d2981cae88cda005d9e8c1b58079fa2
HAJIRNIS Aniket <L6P@3ds.com>
2019-08-16 15:39:13 -0400

    Basic functions

--- python/Automate/3/guess.py
+++ python/Automate/3/guess.py
@@ -1,4 +1,20 @@
 import random
 
 secretNumber = random.randint(1,20)
-print('I am thinking of :'+secretNumber)
+# print('I am thinking of : '+str(secretNumber))
+# print('Hi')
+
+def takeAGuess(guess):
+    if guess < secretNumber:
+        return 'Too low!!'
+    elif guess > secretNumber:
+        return 'Too high!!'
+    return 'Got it!'
+
+print('I am thinking of a number between 1 and 20')
+for i in range(1,7):
+    print('Guess??')
+    guess = int (input())
+    print(takeAGuess(guess))
+if (guess != secretNumber):
+    print('Oops! You missed after 6 tries')
