961eee59a93ed169e3252104c035f37581a2c397
Aniket Hajirnis <anikethajirnis@gmail.com>
2020-03-10 20:20:14 -0400

    Ping and Proxy comparison test

--- Recursive_REST/src/test/java/me/nxs/rest/RESTTestTest.java
+++ Recursive_REST/src/test/java/me/nxs/rest/RESTTestTest.java
@@ -1,13 +1,12 @@
 package me.nxs.rest;
 
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.junit.InSequence;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -27,16 +26,19 @@ public class RESTTestTest {
     private URL webappURL;
 
     WebTarget webTarget;
+    WebTarget pingTarget;
+    WebTarget proxyTarget;
 
     @Before
     public void before() throws Exception{
-        webTarget = ClientBuilder.newClient().target(webappURL.toURI()).path("RecREST");
-        System.out.println(webappURL.getHost()+webappURL.getPort());
-//        webTarget = ClientBuilder.newClient().target(webappURL.toURI());
+        webTarget = ClientBuilder.newClient().target(webappURL.toURI());
+        pingTarget = webTarget.path("ping");
+        proxyTarget = webTarget.path("proxy");
     }
-    @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
+
+    @Deployment(testable = false)
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class, "RecREST.war")
                 .addClass(RESTTest.class)
                 .addClass(RestAggregator.class)
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
@@ -45,15 +47,26 @@ public class RESTTestTest {
 
     @Test
     @InSequence(1)
-    @RunAsClient
-    public void checkPingStatus() throws Exception{
-        final Response response =  webTarget
-                .path("ping")
+    public void checkPing(){
+        Response response =  pingTarget
                 .request()
                 .get();
-
         assertEquals(Response.Status.OK.getStatusCode(),response.getStatus());
+        assertEquals("Hello World!!",response.readEntity(String.class));
 
     }
+
+    @Test
+    @InSequence(2)
+    public void checkPingMessage(){
+        Response pingResponse = pingTarget.request().get();
+        Response proxyResponse = proxyTarget.request().get();
+        assertEquals(Response.Status.OK.getStatusCode(),proxyResponse.getStatus());
+        assertNotEquals(pingResponse,proxyResponse);
+        assertNotEquals(pingResponse.readEntity(String.class),proxyResponse.readEntity(String.class));
+        assertEquals(pingResponse.readEntity(String.class).concat(", Aniket"),proxyResponse.readEntity(String.class));
+
+    }
+
 
 }
